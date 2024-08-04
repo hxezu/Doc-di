313a8752d4cf90829d67c0fb840371c7d8ebda4b
hxezu <hailey_apple@icloud.com>
2024-08-05 01:50:41 +0900

    Dose Task UI Edited

--- app/src/main/java/com/example/doc_di/management/home/MedicationCard.kt
+++ app/src/main/java/com/example/doc_di/management/home/MedicationCard.kt
@@ -1,9 +1,15 @@
 package com.example.doc_di.management.home
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
@@ -17,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,71 +43,76 @@ fun MedicationCard(
     medication: Medication,
     navigateToMedicationDetail: (Medication) -> Unit
 ) {
-
-    Card(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 10.dp, horizontal = 20.dp),
-        onClick = { navigateToMedicationDetail(medication) },
-        shape = RoundedCornerShape(30.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = LightBlue,
-        )
+
     ) {
 
-        Row(
-            modifier = Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
+        Text(medication.medicationTime.toFormattedTimeString())
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(vertical = 10.dp, horizontal = 20.dp)
+                .border(
+                    border = BorderStroke(width = 1.dp, color = Color.Gray), // Black border with 2.dp width
+                    shape = RoundedCornerShape(30.dp) // Same shape as the card
+                ),
+            onClick = { navigateToMedicationDetail(medication) },
+            shape = RoundedCornerShape(30.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White,
+            )
         ) {
 
-            Column(
-                modifier = Modifier
-                    .weight(2f),
-                horizontalAlignment = Alignment.Start
+            Row(
+                modifier = Modifier.padding(16.dp),
+                verticalAlignment = Alignment
+                    .CenterVertically
             ) {
-                Text(
-                    modifier = Modifier.padding(bottom = 16.dp),
-                    style = MaterialTheme.typography.titleSmall,
-                    text = medication.medicationTime.toFormattedDateString().uppercase(),
-                    color = MainBlue
+                Image(
+                    modifier = Modifier
+                        .padding(start = 15.dp)
+                        .align(Alignment.CenterVertically)
+                        .weight(1f),
+                    painter = painterResource(id = R.drawable.pill),
+                    contentDescription = "용법 이미지"
                 )
 
-                Text(
-                    text = medication.name,
-                    fontWeight = FontWeight.Bold,
-                    style = MaterialTheme.typography.titleLarge
-                )
+                Spacer(modifier = Modifier.width(20.dp))
 
-                val medicationStatusText = when {
-                    medication.medicationTime.hasPassed() -> {
-                        if (medication.medicationTaken) {
-                            stringResource(
-                                id = R.string.medication_taken_at,
-                                medication.medicationTime.toFormattedTimeString()
-                            )
-                        } else {
-                            stringResource(
-                                id = R.string.medication_skipped_at,
-                                medication.medicationTime.toFormattedTimeString()
-                            )
-                        }
-                    }
-
-                    else -> stringResource(
-                        id = R.string.medication_scheduled_at,
-                        medication.medicationTime.toFormattedTimeString()
+                Column(
+                    modifier = Modifier
+                        .weight(3f),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = medication.name,
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.titleSmall
                     )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = medication.dosage.toString() + " 정",
+                        fontWeight = FontWeight.Medium,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+
                 }
 
                 Text(
-                    text = medicationStatusText,
-                    color = Color.Black
+                    text = medication.recurrence + " 일간",
+                    fontWeight = FontWeight.Medium,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier
+                        .weight(1.5f)
                 )
             }
-
-            Icon(imageVector = Icons.Default.KeyboardArrowRight, contentDescription = null)
         }
     }
+
 }
 
 @Preview
@@ -109,7 +121,7 @@ private fun MedicationCardTakeNowPreview() {
     MedicationCard(
         Medication(
             id = 123L,
-            name = "A big big name for a little medication I needs to take",
+            name = "오메가 3",
             dosage = 1,
             recurrence = "2",
             endDate = Date(),
@@ -125,7 +137,7 @@ private fun MedicationCardTakenPreview() {
     MedicationCard(
         Medication(
             id = 123L,
-            name = "A big big name for a little medication I needs to take",
+            name = "소염제",
             dosage = 1,
             recurrence = "2",
             endDate = Date(),
