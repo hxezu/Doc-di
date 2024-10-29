29e94ead7f2322094ce8df70e20087415f345b72
flffkd12 <flffkd12@gmail.com>
2024-10-30 03:17:34 +0900

    검색탭 카드 이미지 수정

--- app/src/main/java/com/example/doc_di/domain/RetrofitInstance.kt
+++ app/src/main/java/com/example/doc_di/domain/RetrofitInstance.kt
@@ -16,8 +16,8 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
 
-    //const val BASE_URL = "http://192.168.53.189:8080/"
-    const val BASE_URL = "http://192.168.84.213:8080/"
+    const val BASE_URL = "http://192.168.53.189:8080/"
+    //const val BASE_URL = "http://192.168.0.7:8080/"
 
     private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
